0e81aa8a47fff20f1925e0ba1b2a84fae60f4256
Heidrun Kubiessa <heidrunkubiessa@heidruns-air.lan>
2022-12-04 16:12:23 -0700

    moved files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const api = require('./Develop/routes/index.js');
+const PORT = process.env.port || 3001;
+const app = express();
+
+//express use method
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api' , api),
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//HTML routes
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+//Port activated
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
