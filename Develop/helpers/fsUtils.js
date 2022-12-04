e12f202d01abe069d9d0e870a3c29237dc966abf
Heidrun Kubiessa <heidrunkubiessa@heidruns-air.lan>
2022-12-04 09:39:36 -0700

    still figuring out the DELETE part

--- Develop/helpers/fsUtils.js
+++ Develop/helpers/fsUtils.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const util = require('util');
+let db = require('../db/db.json')
 
 
 // Promise version of fs.readFile
@@ -13,16 +14,41 @@ fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => err ? conso
 // Function to read notes from file and append a new note
  
 const readAndAppend = (content, file) => {
+    const note = content;
     fs.readFile(file, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             
     }else{
         const parsedData = JSON.parse(data);
-        parsedData.push(content);
+        parsedData.push(note);
         writeToFile(file, parsedData);
     }
     });
 };
+const deleteNote = (id) => {
+    return new Promise( (resolve, reject) => {
+        resolve('test')
+    })
 
-module.exports = { readFromFile, writeToFile, readAndAppend };
+    };
+    // let savedNotes = notes.filter(el => {
+    //     if(el.id == id) {
+    //         return false
+    //     }else{
+    //         return true
+    //     }
+        // let index = 0;
+        // savedNotes.forEach(note => {
+        //     note.id = index;
+        //     index +=1; 
+
+        
+        //);
+        // fs.writeFile(destination, JSON.stringify({savedNotes}, null, 4));
+        // return savedNotes
+   // }
+    //)
+//};
+
+module.exports = { readFromFile, writeToFile, readAndAppend, deleteNote };
--- Develop/routes/notesAPI.js
+++ Develop/routes/notesAPI.js
@@ -1,8 +1,6 @@
 const notesAPI = require('express').Router();
-const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile, readAndAppend, deleteNote } = require('../helpers/fsUtils');
 const savedNotes = require('../db/db.json');
- 
- 
 
 let id = savedNotes.length;
 //notesAPI.use('/db/db.json', savedNotes)
@@ -25,7 +23,6 @@ notesAPI.post('/', (req, res) => {
             title,
             text,
             id,
-
         };
     
     readAndAppend(newNote, './db/db.json');
@@ -38,11 +35,34 @@ notesAPI.post('/', (req, res) => {
     res.json('Error in creating new note');
 }
 });
-notesAPI.delete('/api/notes/:id', (req, res) => {
+notesAPI.delete('/:id', (req, res) => {
     const { id } = req.params;
-    savedNotesIndex = savedNotes.findIndex(newNote => newNote.id === id);
-    savedNotes.splice(savedNotesIndex, 1);
-    return res.send();
-})
+//     savedNotes = deleteNote(id, savedNotes);
+//     console.log('savedNotes: ', savedNotes);
+//    // console.log('savedNoteIndex: ', savedNotesIndex);
+//     res.json(savedNotes);
+    // let index = 0;
+    //     savedNotes.forEach(note => {
+    //         note.id = index;
+    //         index +=1; 
+            
+    // savedNotesIndex = savedNotes.findIndex(newNote => newNote.id === id);
+    // savedNotes.splice(savedNotesIndex, 1);
+    //readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+   // writeToFile('./db/db.json');
+
+    
+    //return res.send(savedNotes);
+    deleteNote(id)
+        .then(() => {
+            return res.json({message: 'note deleted successfully'})
+
+
+
+        })
+        .catch((err) => {
+            return res.status(500).json(err)
+        });
+});
 
 module.exports = notesAPI;
