69ed57555a99142f0949b9015a75a37ccc1a2a3f
Heidrun Kubiessa <heidrunkubiessa@heidruns-air.lan>
2022-12-04 18:14:41 -0700

    PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./Develop/routes/index.js');
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 //express use method
